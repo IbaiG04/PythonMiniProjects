c933fdc9ef0ea2ad8600ac70c0987a493af1db47
IbaiG04 <97726677+IbaiG04@users.noreply.github.com>
2024-03-25 11:20:50 +0100

    Update QRCodeCreator.py

--- MiniPythonProjects/QRCodeCreator.py
+++ MiniPythonProjects/QRCodeCreator.py
@@ -5,7 +5,7 @@ data = input("Escribe los datos que quieres que contenga el QR: ")
 
 img = qrcode.make(data)
 
-nombre_archivo = f"D:/coding/Python/PythonLearn/MiniPythonProjects/qr/myqrcode.png"
+nombre_archivo = "YOUR DIRECTORY"
 img.save(nombre_archivo)
 
 print("QR creado con éxito!")
